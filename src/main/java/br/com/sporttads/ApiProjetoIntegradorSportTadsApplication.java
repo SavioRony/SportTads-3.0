285c9770d15a761433581227f7b80da06ac305a7
Hugo Souza Araujo <hsaraujo98@outlook.com>
2021-02-26 16:28:37 -0300

    criação do projeto, definição da estrutura e configuração do banco de dados H2

---
+++ src/main/java/br/com/sporttads/ApiProjetoIntegradorSportTadsApplication.java
@@ -0,0 +1,14 @@
+package br.com.sporttads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjetoIntegradorSportTadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjetoIntegradorSportTadsApplication.class, args);
+		System.out.println("API RODANDO!");
+	}
+
+}
---
+++ src/test/java/br/com/sporttads/ApiProjetoIntegradorSportTadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sporttads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoIntegradorSportTadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
